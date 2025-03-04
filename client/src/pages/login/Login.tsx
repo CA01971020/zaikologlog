bed848d467e2b96efe1ae3764ae5fd56298a60ce
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-03-04 09:55:39 +0900

    Update Login.tsx

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,11 +1,11 @@
 function Login() {
   return (
     <div>
-      <div className=" justify-center w-full text-center mt-10 bg-dull-white">
+      <div className=" justify-center w-full text-center mt-10">
         <h1 className="text-3xl">家庭用在庫管理アプリ</h1>
         <h2 className="text-4xl mt-5">在庫ログログ</h2>
       </div>
-      <div></div>
+      <div className="mt-20 bg-dull-white">a</div>
     </div>
   );
 }
