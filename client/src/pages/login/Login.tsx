60f45b02d325b5d599c1a7a11e04bfafa8b7ba1e
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-03-10 02:22:17 +0900

    ログイン画面の更新（仮）

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,11 +1,32 @@
+import { Link } from "react-router-dom";
+import LoginButton from "../../components/ui/LoginButton";
+import FirstHeader from "../../components/base/FirstHeader";
+
 function Login() {
   return (
     <div>
-      <div className=" justify-center w-full text-center mt-10">
-        <h1 className="text-3xl">家庭用在庫管理アプリ</h1>
-        <h2 className="text-4xl mt-5">在庫ログログ</h2>
+      <FirstHeader />
+      <div className="bg-dull-white rounded-2xl border mt-12 pt-12 pb-12 ml-6 mr-6">
+        <h3 className="text-center">アカウントをお持ちの方</h3>
+
+        <div className="flex justify-center items-center mt-6">
+          <div className="flex flex-col items-start gap-4">
+            <p>メールアドレス</p>
+            <p>パスワード</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-6 mt-6 ml-12 mr-12">
+          <LoginButton buttontitle="ログイン" />
+          <LoginButton buttontitle="Googleログイン" />
+        </div>
+
+        <h3 className="text-center mt-12">アカウントをお持ちでない場合</h3>
+
+        <p className="text-blue text-center mt-4">
+          <Link to="/Signup">新規登録</Link>
+        </p>
       </div>
-      <div className="mt-20 bg-dull-white">a</div>
     </div>
   );
 }
