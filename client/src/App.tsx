2968de3088b89a1e530e1f06483a0c8baa004684
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-03-10 02:20:52 +0900

    ReactRouterの設定

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Signup from "./pages/login/Signup";
+import Footer from "./components/base/Footer";
+import NotFound from "./pages/NotFound";
+import "./index.css";
 
 function App() {
-  return <Login />;
+  return (
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
+  );
 }
 
 export default App;
