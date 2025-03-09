851b5d000b1680b53017400e32bac91fc9c41bcc
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-03-10 02:20:36 +0900

    Create Footer.tsx

---
+++ client/src/components/base/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="fixed bottom-6 left-1/2 transform -translate-x-1/2">
+      <p className="copyright">&copy; Zaiko-LogLog 2025.</p>
+    </footer>
+  );
+}
+
+export default Footer;
